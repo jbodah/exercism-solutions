76b46e671235f8cf34d135981ea366b658b0c77a
Josh Bodah <jb3689@yahoo.com>
2016-03-28 22:20:34 -0400

    elixir 1 + 2

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,34 @@
 defmodule Teenager do
-  def hey(input) do
-    cond do
+  @moduledoc """
+  Bob is a lackadaisical teenager
 
+  Bob answers 'Sure.' if you ask him a question.
+
+  He answers 'Woah, chill out!' if you yell at him.
+
+  He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+  He answers 'Whatever.' to anything else.
+  """
+
+  def hey(string) do
+    cond do
+      silence?(string)  -> "Fine. Be that way!"
+      yelling?(string)  -> "Woah, chill out!"
+      question?(string) -> "Sure."
+      true              -> "Whatever."
     end
+  end
+
+  defp yelling?(string) do
+    String.upcase(string) == string && String.match?(string, ~r/[\p{L}a-zA-Z]+/)
+  end
+
+  defp question?(string) do
+    string |> String.last == "?"
+  end
+
+  defp silence?(string) do
+    string |> String.match?(~r/^\s*$/)
   end
 end
